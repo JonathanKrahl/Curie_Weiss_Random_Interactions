cf4c49ab58008311ca5361151b9d84f2977b6e94
JonathanKrahl <66637949+JonathanKrahl@users.noreply.github.com>
2024-06-13 20:43:55 +0200

    Update proof_of_concept.py

--- source/proof_of_concept.py
+++ source/proof_of_concept.py
@@ -10,6 +10,6 @@ from statistics import mode
 from metropolis import cw_metropolis
 
 
-for i in np.arange(10,11):
+for i in np.arange(10,100,10):
     cw, mag_d_array, burn_in, steps, xi = cw_metropolis(False, 0, 0.3, i, 2, 1000000)
     print(str(mode(mag_d_array))[:7])
